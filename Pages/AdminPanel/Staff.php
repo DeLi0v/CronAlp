71a2d8b1b7a7d59878101e8f72034ebf34a2b115
DeLi0v <denov78@mail.ru>
2023-05-02 23:36:54 +0300

    modified:   Pages/AdminPanel/Staff.php

--- Pages/AdminPanel/Staff.php
+++ Pages/AdminPanel/Staff.php
@@ -1,3 +1,17 @@
-<?php 
-echo "В разработке";
-?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>MySiTe</title>
+    <link rel="stylesheet" href="Styles/MainStyles.css">
+</head>
+
+<body>
+    <?php require("/head.php"); ?>
+    <?php 
+    echo "В разработке";
+    ?>
+</body>
+
+</html>
