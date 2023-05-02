fec0252db60075463afa0057e9547b994cb71630
DeLi0v <denov78@mail.ru>
2023-05-02 23:10:25 +0300

    modified:   Styles/MainStyles.css 	modified:   index.php

--- index.php
+++ index.php
@@ -8,23 +8,9 @@
 </head>
 
 <body>
-    <!-- <?php include "head.php"; ?> -->
-    <div class="head">
-        <h1>Административная панель</h1>
-    </div>
-    <div class="tableHead">
-        <a href="#">Услуги</a>
-        <!-- <a href="/Pages/AdminPanel/Services.php">Услуги</a> -->
-        <a href="#">Клиенты</a>
-        <a href="#">Ski-Pass</a>
-        <a href="#">Сотрудники</a>
-    </div>
+    <?php include "head.php"; ?>
     <div>
-    <a class="btn" href="Pages/AdminPanel/Select.php">
-    <br>
-    <a class="btn" href="Pages/AdminPanel/Edit.php">Изменить</a>
-    <br>
-    <a class="btn" href="Pages/AdminPanel/Add.php">Добавить</a>
+    <p>Добро пожаловать в административную панель, сверху выберете какую таблицу хотите открыть</p>
     </div>
 </body>
 
