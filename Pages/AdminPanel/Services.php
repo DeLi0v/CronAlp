668e7ba8538ac05b6addc06a293b2588f00a7499
DeLi0v <denov78@mail.ru>
2023-05-03 00:22:02 +0300

    modified:   Pages/AdminPanel/Equepment.php 	modified:   Pages/AdminPanel/Services.php 	modified:   Pages/AdminPanel/Ski_Pass.php 	modified:   Pages/AdminPanel/Staff.php

--- Pages/AdminPanel/Services.php
+++ Pages/AdminPanel/Services.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <title>MySiTe</title>
     <link rel="stylesheet" href="/Styles/MainStyles.css">
+    <link rel="stylesheet" href="/Styles/AdminPanelStyles.css">
 </head>
 
 <body class="services">
--- Pages/AdminPanel/Ski_Pass.php
+++ Pages/AdminPanel/Ski_Pass.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <title>MySiTe</title>
-    <link rel="stylesheet" href="Styles/MainStyles.css">
+    <link rel="stylesheet" href="/Styles/MainStyles.css">
+    <link rel="stylesheet" href="/Styles/AdminPanelStyles.css">
 </head>
 
 <body class="ski_pass">
