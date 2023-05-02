1d0639cc79f18df16aad8b303d8559f4cda0f308
DeLi0v <denov78@mail.ru>
2023-05-02 23:26:28 +0300

    modified:   Pages/AdminPanel/Clients.php

--- Pages/AdminPanel/Clients.php
+++ Pages/AdminPanel/Clients.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>MySiTe</title>
-        <link rel="stylesheet" href="/Styles/MainStyles.css">
-    </head>
-    <body>
-        <?php include "../../head.php"; ?>
-        <div>
+
+<head>
+    <meta charset="utf-8">
+    <title>MySiTe</title>
+    <link rel="stylesheet" href="Styles/MainStyles.css">
+</head>
+
+<body>
+        <?php include "/head.php"; ?>
+        <br>
         <?php
             require("Select.php");
             SelectTable("Clients")
