167964a0804cbb00f6e11f78fb1513bdcbcc1b87
DeLi0v <denov78@mail.ru>
2023-05-02 23:16:19 +0300

    modified:   Styles/MainStyles.css 	modified:   head.php

--- head.php
+++ head.php
@@ -12,6 +12,7 @@
         <h1>Административная панель</h1>
     </div>
     <div class="tableHead">
+        <img src="/Pictures/Logo_Untitled.png">
         <a href="/Pages/AdminPanel/Services.php">Услуги</a>
         <a href="/Pages/AdminPanel/Clients.php">Клиенты</a>
         <a href="/Pages/AdminPanel/Ski_Pass.php">Ski-Pass</a>
