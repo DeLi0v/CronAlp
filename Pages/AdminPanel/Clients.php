3aa1b82bcef9028907a6ecf2e97c25850971f9c9
DeLi0v <denov78@mail.ru>
2023-05-02 23:31:40 +0300

    modified:   Pages/AdminPanel/Clients.php 	modified:   Styles/MainStyles.css

--- Pages/AdminPanel/Clients.php
+++ Pages/AdminPanel/Clients.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <title>MySiTe</title>
-    <link rel="stylesheet" href="Styles/MainStyles.css">
-</head>
-
-<body>
         <?php include "../../head.php"; ?>
-        <br>
         <?php
             require("Select.php");
             SelectTable("Clients")
         ?>
-        </div>
-    </body>
-</html>
