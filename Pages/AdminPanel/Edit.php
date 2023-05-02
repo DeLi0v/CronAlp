534d1c75a11282e5d303af41fd432104a5f9d272
DeLi0v <denov78@mail.ru>
2023-05-02 21:39:11 +0300

    modified:   Pages/AdminPanel/Add.php 	modified:   Pages/AdminPanel/Edit.php 	new file:   Pages/AdminPanel/Services.php 	new file:   Pages/AdminPanel/Ski_Pass.php 	new file:   Pages/AdminPanel/Staff.php 	modified:   index.html

--- Pages/AdminPanel/Edit.php
+++ Pages/AdminPanel/Edit.php
@@ -1,21 +1,3 @@
-<?php session_start();
-define("INDEX", ""); // УСТАНОВКА КОНСТАНТЫ ГЛАВНОГО КОНТРОЛЛЕРА
-
-require_once($_SERVER[DOCUMENT_ROOT]."../../connect.php"); // ПОДКЛЮЧЕНИЕ ЯДРА
-
-// ПОДКЛЮЧЕНИЕ К БД
-$db = new MyDB();
-$db->connect();
-
-// ГЛАВНЫЙ КОНТРОЛЛЕР
-switch ($_GET[option]) {
-case "page":
-include($_SERVER[DOCUMENT_ROOT]."page.php");
-break;
-default:
-include($_SERVER[DOCUMENT_ROOT]."home.php");
-break;
-}
-
-include ($_SERVER[DOCUMENT_ROOT]."template.php");
-$db->close();
+<?php 
+echo "В разработке";
+?>
