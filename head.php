9df83dc72e64e32d786b421ff99ea6a67b6c0d0c
DeLi0v <denov78@mail.ru>
2023-05-03 19:31:26 +0300

    modified:   head.php

--- head.php
+++ head.php
@@ -15,9 +15,32 @@
             <a id="nav-services" href="/Pages/AdminPanel/Services.php">Открыть таблицу</a>
             <a href="#">Добавить данные</a>
         </div>
+    </div>
+    <div class="dropdown">
         <button class="dropbtn">Клиенты</button>
         <div class="dropdown-content">
             <a id="nav-clients" href="/Pages/AdminPanel/Clients.php">Открыть таблицу</a>
+            <a href="#">Добавить данные</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="dropbtn">Ski-pass</button>
+        <div class="dropdown-content">
+            <a id="nav-ski_pass" href="/Pages/AdminPanel/Ski_Pass.php">Открыть таблицу</a>
+            <a href="#">Добавить данные</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="dropbtn">Сотрудники</button>
+        <div class="dropdown-content">
+            <a id="nav-staff" href="/Pages/AdminPanel/Staff.php">Открыть таблицу</a>
+            <a href="#">Добавить данные</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="dropbtn">Оборудование</button>
+        <div class="dropdown-content">
+            <a id="nav-equepment" href="/Pages/AdminPanel/Equepment.php">Открыть таблицу</a>
             <a href="#">Добавить данные</a>
         </div>
     </div>
