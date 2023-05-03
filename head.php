1d81a2520612165f264a63d31727b6da330375e1
DeLi0v <denov78@mail.ru>
2023-05-03 19:39:17 +0300

    modified:   Styles/AdminPanelStyles.css 	modified:   head.php 	modified:   index.php

--- head.php
+++ head.php
@@ -1,13 +1,6 @@
 <div class="head">
     <h1>Административная панель</h1>
 </div>
-<!-- <div class="tableHead">
-    <a id="nav-services" href="/Pages/AdminPanel/Services.php">Услуги</a>
-    <a id="nav-clients" href="/Pages/AdminPanel/Clients.php">Клиенты</a>
-    <a id="nav-ski_pass" href="/Pages/AdminPanel/Ski_Pass.php">Ski-Pass</a>
-    <a id="nav-staff" href="/Pages/AdminPanel/Staff.php">Сотрудники</a>
-    <a id="nav-equepment" href="/Pages/AdminPanel/Equepment.php">Оборудование</a>
-</div> -->
 <div class="tableHead">
     <div class="dropdown">
         <button class="dropbtn">Услуги</button>
