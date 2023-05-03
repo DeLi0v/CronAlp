c35a92b0412dc2795929e08906df975db90ea2ac
DeLi0v <denov78@mail.ru>
2023-05-03 19:16:57 +0300

    modified:   head.php

--- head.php
+++ head.php
@@ -1,18 +1,19 @@
 <div class="head">
     <h1>Административная панель</h1>
 </div>
-<div class="tableHead">
+<!-- <div class="tableHead">
     <a id="nav-services" href="/Pages/AdminPanel/Services.php">Услуги</a>
     <a id="nav-clients" href="/Pages/AdminPanel/Clients.php">Клиенты</a>
     <a id="nav-ski_pass" href="/Pages/AdminPanel/Ski_Pass.php">Ski-Pass</a>
     <a id="nav-staff" href="/Pages/AdminPanel/Staff.php">Сотрудники</a>
     <a id="nav-equepment" href="/Pages/AdminPanel/Equepment.php">Оборудование</a>
-</div>
-<div class="dropdown">
-    <button class="dropbtn">Выпадающее</button>
-    <div class="dropdown-content">
-        <a href="#">Ссылка 1</a>
-        <a href="#">Ссылка 2</a>
-        <a href="#">Ссылка 3</a>
+</div> -->
+<div class="tableHead">
+    <div class="dropdown">
+        <button class="dropbtn">Услуги</button>
+        <div class="dropdown-content">
+            <a id="nav-services" href="/Pages/AdminPanel/Services.php">Открыть таблицу</a>
+            <a href="#">Добавить данные</a>
+        </div>
     </div>
 </div>
