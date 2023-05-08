fe5594696caa5f63d840bde5d5a263d05a85e90b
DeLi0v <denov78@mail.ru>
2023-05-08 15:28:48 +0300

    modified:   Styles/AdminPanelStyles.css 	modified:   head.php

--- head.php
+++ head.php
@@ -6,35 +6,35 @@
         <button class="dropbtn" id="nav-services">Услуги</button>
         <div class="dropdown-content">
             <a id="nav-services" href="/Pages/AdminPanel/Select/Services.php">Открыть таблицу</a>
-            <a id="nav-services-add" href="/Pages/AdminPanel/Add/Services.php">Добавить данные</a>
+            <a id="nav-services" href="/Pages/AdminPanel/Add/Services.php">Добавить данные</a>
         </div>
     </div>
     <div class="dropdown">
         <button class="dropbtn" id="nav-clients">Клиенты</button>
         <div class="dropdown-content">
             <a id="nav-clients" href="/Pages/AdminPanel/Select/Clients.php">Открыть таблицу</a>
-            <a id="nav-clients-add" href="/Pages/AdminPanel/Add/Clients.php">Добавить данные</a>
+            <a id="nav-clients" href="/Pages/AdminPanel/Add/Clients.php">Добавить данные</a>
         </div>
     </div>
     <div class="dropdown">
         <button class="dropbtn"id="nav-ski_pass">Ski-pass</button>
         <div class="dropdown-content">
             <a id="nav-ski_pass" href="/Pages/AdminPanel/Select/Ski_Pass.php">Открыть таблицу</a>
-            <a id="nav-ski_pass-add" href="/Pages/AdminPanel/Add/Ski_Pass.php">Добавить данные</a>
+            <a id="nav-ski_pass" href="/Pages/AdminPanel/Add/Ski_Pass.php">Добавить данные</a>
         </div>
     </div>
     <div class="dropdown">
         <button class="dropbtn" id="nav-staff">Сотрудники</button>
         <div class="dropdown-content">
             <a id="nav-staff" href="/Pages/AdminPanel/Select/Staff.php">Открыть таблицу</a>
-            <a id="nav-staff-add" href="/Pages/AdminPanel/Add/Staff.php">Добавить данные</a>
+            <a id="nav-staff" href="/Pages/AdminPanel/Add/Staff.php">Добавить данные</a>
         </div>
     </div>
     <div class="dropdown">
         <button class="dropbtn" id="nav-equepment">Оборудование</button>
         <div class="dropdown-content">
             <a id="nav-equepment" href="/Pages/AdminPanel/Select/Equepment.php">Открыть таблицу</a>
-            <a id="nav-equepment-add" href="/Pages/AdminPanel/Add/Equepment.php">Добавить данные</a>
+            <a id="nav-equepment" href="/Pages/AdminPanel/Add/Equepment.php">Добавить данные</a>
         </div>
     </div>
 </div>
