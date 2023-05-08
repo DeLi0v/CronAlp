3069921d1546fa8c275d5adaff751e8f656b5559
DeLi0v <denov78@mail.ru>
2023-05-08 16:47:10 +0300

    modified:   Pages/AdminPanel/Add/Ski_Pass.php

--- Pages/AdminPanel/Add/Ski_Pass.php
+++ Pages/AdminPanel/Add/Ski_Pass.php
@@ -14,28 +14,29 @@
     <form action="/Pages/AdminPanel/Add/ClientsAdd.php" method="post" style=" margin:auto; width:500px;">
         <ul class="wrapper">
             <li class="form-row">
+                <label for="ClientSurname">Клиент, которому выдается ski-pass:</label>
                 <?php 
-                require_once("../../../connect.php"); // Подключение файла для связи с БД
+                    require_once("../../../connect.php"); // Подключение файла для связи с БД
 
-                // // Подключение к БД
-                $db = new DB_Class();
-                $conn = $db->connect();
-                mysqli_select_db($conn, $db->database);
+                    // // Подключение к БД
+                    $db = new DB_Class();
+                    $conn = $db->connect();
+                    mysqli_select_db($conn, $db->database);
 
-                // Формируем SQL-запрос для получения данных из таблицы "users"
-                $sql = "SELECT * FROM Clients";
+                    // Формируем SQL-запрос для получения данных из таблицы "users"
+                    $sql = "SELECT * FROM Clients";
 
-                // Выполняем SQL-запрос
-                $result = mysqli_query($conn, $sql);
-                
-                /*Выпадающий список*/
-                echo "<select name=\"Client\">";
-                
-                while($object = mysqli_fetch_object($result)){
-                    echo "<option value = '$object->ClientSurname' > $object->idClient $object->ClientSurname </option>";
-                }
-                
-                echo "</select>";
+                    // Выполняем SQL-запрос
+                    $result = mysqli_query($conn, $sql);
+                    
+                    /*Выпадающий список*/
+                    echo "<select name=\"Client\">";
+                    
+                    while($object = mysqli_fetch_object($result)){
+                        echo "<option value = '$object->ClientSurname' > $object->idClient - $object->ClientSurname $object->ClientName $object->ClientOtch</option>";
+                    }
+                    
+                    echo "</select>";
                 ?>
             </li>
             <li class="form-row">
