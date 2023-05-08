b28dadb57396fc0cfa489daafb9514452d73f8b5
DeLi0v <denov78@mail.ru>
2023-05-08 15:02:09 +0300

    Changes to be committed: 	modified:   Pages/AdminPanel/Add/Clients.php
    Changes not staged for commit:
    	modified:   Styles/AdminPanelStyles.css

--- Pages/AdminPanel/Add/Clients.php
+++ Pages/AdminPanel/Add/Clients.php
@@ -12,33 +12,35 @@
     <?php include("../../../head.php"); ?>
     <h3>Добавление пользователя</h3>
     <form action="/Pages/AdminPanel/Add/ClientsAdd.php" method="post">
-        <div class="wrapper">
-            <p class="form-row">
+        <ul class="wrapper">
+            <li class="form-row">
                 <label for="ClientSurname">Фамилия:</label>
                 <input type="text" name="ClientSurname" size="20px" />
-            </p>
-            <p>
+            </li>
+            <li class="form-row">
                 <label for="ClientName">Имя:</label>
                 <input type="text" name="ClientName" size="20px" />
-            </p>
-            <p class="form-row">
+            </li>
+            <li class="form-row">
                 <label for="ClientOtch">Отчетсво:</label>
                 <input type="text" name="ClientOtch" size="20px" />
-            </p>
-            <p class="form-row">
+            </li>
+            <li class="form-row">
                 <label for="Phone">Телефон:</label>
                 <input type="text" name="Phone" size="20px" />
-            </p>
-            <p class="form-row">
+            </li>
+            <li class="form-row">
                 <label for="Mail">Почта:</label>
                 <input type="text" name="Mail" size="20px" />
-            </p>
-            <p class="form-row">
+            </li>
+            <li class="form-row">
                 <label for="Passwd">Пароль:</label>
                 <input type="text" name="Passwd" size="20px" />
-            </p>
-            <input class="form-row" type="submit" value="Добавить">
-        </div>
+            </li>
+            <li class="form-row">
+                <button type="submit">Добавить</button>
+            </li>
+        </ul>
     </form>
 </body>
 
