e36c6efc32603168684d2301fbf6cce50b0a16b4
DeLi0v <denov78@mail.ru>
2023-05-10 20:00:25 +0300

    modified:   Pages/AdminPanel/Select/Select.php 	new file:   pictures/icons/trash-full.png

--- Pages/AdminPanel/Select/Select.php
+++ Pages/AdminPanel/Select/Select.php
@@ -222,7 +222,7 @@ function Equepments($result){
                 <form action='/Pages/AdminPanel/Delete.php?id=\"".$row["id"]."\"' method=\"post\">
                     <input type=\"hidden\" name=\"id\" value=\"".$row["id"]."\">
                     <input type=\"hidden\" name=\"page\" value=\"Equepment\">
-                    <input type=\"image\" name=\"submit\" value=\"Delete\" src=\"/pictures/icons/trash.png\" style=\"max-width: 35px;\">
+                    <input type=\"image\" name=\"submit\" value=\"Delete\" src=\"/pictures/icons/trash-full.png\" style=\"max-width: 25px;\">
                 </form>
               </td>";
         echo "</tr>";
