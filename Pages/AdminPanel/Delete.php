f214ebadd0ec49a69e55710bca31aae2af53a7f0
DeLi0v <denov78@mail.ru>
2023-05-10 19:16:18 +0300

    modified:   Pages/AdminPanel/Delete.php

--- Pages/AdminPanel/Delete.php
+++ Pages/AdminPanel/Delete.php
@@ -11,8 +11,10 @@ if (isset($_POST["id"]) && isset($_POST["table"])) {
     $table = $_POST["table"];
     $sql = "DELETE FROM Ski_pass WHERE (idSki_pass = $id);";
     mysqli_query($conn,$sql);
-    if(!$conn->query($sql)){
-        echo "Ошибка: " . $conn->error;    
+    if($conn->query($sql)){
+        echo "Все ок";     
+    } else {
+        echo "Ошибка: " . $conn->error;   
     }
 } else {
     echo "Что-то не так";
