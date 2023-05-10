ae2834a08649f215c2827a3404148e8428bdba5c
DeLi0v <denov78@mail.ru>
2023-05-10 18:46:25 +0300

    modified:   Pages/AdminPanel/Delete.php

--- Pages/AdminPanel/Delete.php
+++ Pages/AdminPanel/Delete.php
@@ -1,6 +1,6 @@
 <?php 
 if (isset($_POST["Equepment"])) {
-    $id = isset($_POST["Equepment"]);
+    $id = $_POST["Equepment"];
     $sql = "DELETE FROM `mydb`.`Ski_pass` WHERE (`idSki_pass` = '$id');";
 } else {
     echo "Что-то не так";
