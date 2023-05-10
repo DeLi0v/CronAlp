1c11c87ace0ff0ddb4520490b1503d0dcd669c47
DeLi0v <denov78@mail.ru>
2023-05-10 20:40:35 +0300

    modified:   Pages/AdminPanel/Select/EditAndDeleteRows.php

--- Pages/AdminPanel/Select/EditAndDeleteRows.php
+++ Pages/AdminPanel/Select/EditAndDeleteRows.php
@@ -3,7 +3,7 @@ echo "<td class=\"center\">
 <form action='/Pages/AdminPanel/Edit.php?id=\"".$row["id"]."\"' method=\"post\">
     <input type=\"hidden\" name=\"id\" value=\"".$row["id"]."\">
     <input type=\"hidden\" name=\"page\" value=\"$page\">
-    <input type=\"image\" name=\"submit\" value=\"Edit\" src=\"/pictures/icons/edit-orange.png\" style=\"max-width: 30px;\">
+    <input type=\"image\" name=\"submit\" value=\"Edit\" src=\"/pictures/icons/edit_orange.png\" style=\"max-width: 30px;\">
 </form>
 </td>";
 echo "<td class=\"center\">
