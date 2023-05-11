1b0967b4049ed54e7ebfa2e492e2fb2794dccd7a
DeLi0v <denov78@mail.ru>
2023-05-12 00:44:06 +0300

    modified:   Pages/AdminPanel/Delete.php

--- Pages/AdminPanel/Delete.php
+++ Pages/AdminPanel/Delete.php
@@ -16,7 +16,44 @@ if (isset($_POST["id"]) && isset($_POST["page"])) {
         if(!$conn->query($sql)){
             echo "Ошибка: " . $conn->error;   
         }
+    } elseif ($page == "Clients") {
+        $sql = "DELETE FROM Clients WHERE (idClient = $id);";
+        mysqli_query($conn,$sql);
+        if(!$conn->query($sql)){
+            echo "Ошибка: " . $conn->error;   
+        }
+    } elseif ($page == "EquepmentCategories") {
+        $sql = "DELETE FROM EquepmentCategories WHERE (idEquepmentCategory = $id);";
+        mysqli_query($conn,$sql);
+        if(!$conn->query($sql)){
+            echo "Ошибка: " . $conn->error;   
+        }
+    } elseif ($page == "OperationTypes") {
+        $sql = "DELETE FROM OperationTypes WHERE (idOperationType = $id);";
+        mysqli_query($conn,$sql);
+        if(!$conn->query($sql)){
+            echo "Ошибка: " . $conn->error;   
+        }
+    } elseif ($page == "Services") {
+        $sql = "DELETE FROM Services WHERE (idService = $id);";
+        mysqli_query($conn,$sql);
+        if(!$conn->query($sql)){
+            echo "Ошибка: " . $conn->error;   
+        }
+    } elseif ($page == "Ski_Pass") {
+        $sql = "DELETE FROM Ski_pass WHERE (idSki_pass = $id);";
+        mysqli_query($conn,$sql);
+        if(!$conn->query($sql)){
+            echo "Ошибка: " . $conn->error;   
+        }
+    } elseif ($page == "Staff") {
+        $sql = "DELETE FROM Staff WHERE (idStaff = $id);";
+        mysqli_query($conn,$sql);
+        if(!$conn->query($sql)){
+            echo "Ошибка: " . $conn->error;   
+        }
     }
+
 } else {
     echo "Что-то не так";
 }
