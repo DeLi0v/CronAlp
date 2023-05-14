f0b5241ce123593f513698b7d6f5c002c57f07a0
DeLi0v <denov78@mail.ru>
2023-05-14 23:02:32 +0300

    modified:   graph.php

--- graph.php
+++ graph.php
@@ -43,7 +43,7 @@
             $result = mysqli_query($conn, $sql);
             if (mysqli_num_rows($result) > 0) {
                 while ($row = mysqli_fetch_assoc($result)) {
-                echo "['" . $row["data"] . "'," . $row["name"] . ", " . $row["count"] . "],";
+                echo "['" . $row["name"] . "'," . $row["data"] . ", " . $row["count"] . "],";
             }
             } ?>
         ]);
