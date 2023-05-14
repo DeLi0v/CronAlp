4b709f1ada8afadca93d525657b4a276114ea667
DeLi0v <denov78@mail.ru>
2023-05-14 19:56:20 +0300

    Changes to be committed: 	modified:   graph.php

--- graph.php
+++ graph.php
@@ -52,7 +52,7 @@
 
         // Create a range slider, passing some options
         var donutRangeSlider = new google.visualization.ControlWrapper({
-          'controlType': 'NumberRangeFilter',
+          'controlType': 'CategoryFilter',
           'containerId': 'filter_div',
           'options': {
             'filterColumnLabel': 'Количество'
@@ -80,51 +80,6 @@
         dashboard.draw(data);
       }
     </script>
-
-<script type="text/javascript">
-      google.charts.load('current', {'packages':['bar']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Дата', 'Количество'],
-          <?php require_once("connect.php"); // Подключение файла для связи с БД
-            // Подключение к БД
-            $db = new DB_Class();
-            $conn = $db->connect();
-            mysqli_select_db($conn, $db->database);
-            
-            // Запрос
-            $sql = "SELECT 
-                        count(idEquepment) count,
-                        DATE_FORMAT(ServiceData, '%d.%m.%Y') data
-                    FROM Services
-                    WHERE
-                        idOperation = '1'
-                        AND idEquepment = '1'
-                    GROUP BY data";
-
-            // Выполняем SQL-запрос
-            $result = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                echo "['" . $row["data"] . "', ". $row["count"] . "],";
-            }
-            } ?>
-        ]);
-
-        var options = {
-          chart: {
-            title: 'Количество выданного оборудования',
-            //subtitle: 'Sales, Expenses, and Profit: 2014-2017',
-          }
-        };
-
-        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
-
-        chart.draw(data, google.charts.Bar.convertOptions(options));
-      }
-    </script>
   </head>
 
   <body>
@@ -135,6 +90,5 @@
       <div id="filter_div"></div>
       <div id="chart_div"></div>
     </div>
-    <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
   </body>
 </html>
