75f5a99038dfabc624abd48f041fb6a5e7200eed
DeLi0v <denov78@mail.ru>
2023-05-14 16:23:39 +0300

    modified:   graph.php

--- graph.php
+++ graph.php
@@ -1,7 +1,7 @@
 <?php  
  // Standard inclusions        
- include("../lib/pChart/pData.class");     
- include("../lib/pChart/pChart.class");     
+ include("pChart2.1.4/pData.class");     
+ include("pChart2.1.4/pChart.class");     
    
  // Dataset definition      
  $DataSet = new pData;     
