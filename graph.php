840e038e55e37aebb01785f856fccfa0f73254de
DeLi0v <denov78@mail.ru>
2023-05-14 16:41:42 +0300

    modified:   graph.php

--- graph.php
+++ graph.php
@@ -1,25 +1,49 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="http://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
-        <script src="http://cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
-        <script>
-            new Chartist.Line('.chart1', {
-                labels: ['День 1', 'День 2', 'День 3', 'День 4', 'День 5'],
-                    series: [
-                        [12, 9, 3, 8, 4],
-                        [2, 1, 4.7, 5.5, 8]
-                    ]
-                }, {
-                    fullWidth: true,
-                    chartPadding: {
-                        right: 50
-                    }
-            });
-        </script>
-    </head>
 
-    <body>
-        <?php include("head.php") ?>
-        <div class="ct-chart ct-golden-section"></div>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <title>MySiTe</title>
+    <link rel="stylesheet" href="Styles/MainStyles.css">
+    <link rel="stylesheet" href="Styles/AdminPanelStyles.css">
+    <link rel="stylesheet" href="http://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
+    <script src="http://cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
+</head>
+
+<body>
+    <?php include "head.php"; ?>
+    <div class="ct-chart ct-golden-section"></div>
+    <div class="chart1 ct-chart ct-golden-section">
+        <svg xmlns:ct="http://gionkunz.github.com/chartist-js/ct" width="100%" height="100%" class="ct-chart-line" style="width: 100%; height: 100%;">
+            <g class="ct-grids">
+                <line x1="50" x2="50" y1="15" y2="397.625" class="ct-grid ct-horizontal"></line>
+                <line x1="200" x2="200" y1="15" y2="397.625" class="ct-grid ct-horizontal"></line>
+                <line x1="350" x2="350" y1="15" y2="397.625" class="ct-grid ct-horizontal"></line>
+                <line x1="500" x2="500" y1="15" y2="397.625" class="ct-grid ct-horizontal"></line>
+                <line x1="650" x2="650" y1="15" y2="397.625" class="ct-grid ct-horizontal"></line>
+                <line y1="397.625" y2="397.625" x1="50" x2="650" class="ct-grid ct-vertical"></line>
+                <line y1="359.3625" y2="359.3625" x1="50" x2="650" class="ct-grid ct-vertical"></line>
+                <line y1="321.1" y2="321.1" x1="50" x2="650" class="ct-grid ct-vertical"></line>
+                <line y1="282.8375" y2="282.8375" x1="50" x2="650" class="ct-grid ct-vertical"></line>
+                <line y1="244.575" y2="244.575" x1="50" x2="650" class="ct-grid ct-vertical"></line>
+                <line y1="206.3125" y2="206.3125" x1="50" x2="650" class="ct-grid ct-vertical"></line>
+                <line y1="168.05" y2="168.05" x1="50" x2="650" class="ct-grid ct-vertical"></line>
+                <line y1="129.78750000000002" y2="129.78750000000002" x1="50" x2="650" class="ct-grid ct-vertical"></line>
+                <line y1="91.52499999999998" y2="91.52499999999998" x1="50" x2="650" class="ct-grid ct-vertical"></line>
+                <line y1="53.26249999999999" y2="53.26249999999999" x1="50" x2="650" class="ct-grid ct-vertical"></line>
+                <line y1="15" y2="15" x1="50" x2="650" class="ct-grid ct-vertical"></line>
+            </g>
+            <g>
+                <g class="ct-series ct-series-a">
+                    <path d="M50,30.305C100,60.915,150,81.322,200,122.135C250,162.948,300,305.795,350,305.795C400,305.795,450,152.745,500,152.745C550,152.745,600,234.372,650,275.185" class="ct-line"></path>
+                    <line x1="50" y1="30.305000000000007" x2="50.01" y2="30.305000000000007" class="ct-point" ct:value="12"></line>
+                    <line x1="200" y1="122.13499999999999" x2="200.01" y2="122.13499999999999" class="ct-point" ct:value="9"></line>
+                    <line x1="350" y1="305.795" x2="350.01" y2="305.795" class="ct-point" ct:value="3"></line><line x1="500" y1="152.745" x2="500.01" y2="152.745" class="ct-point" ct:value="8"></line>
+                    <line x1="650" y1="275.185" x2="650.01" y2="275.185" class="ct-point" ct:value="4"></line>
+                </g>
+                <g class="ct-series ct-series-b">
+                    <path d="M50,336.405C100,346.608,150,367.015,200,367.015C250,367.015,300,267.181,350,253.758C400,240.335,450,241.638,500,229.27C550,216.902,600,178.253,650,152.745" class="ct-line"></path>
+                    <line x1="50" y1="336.405" x2="50.01" y2="336.405" class="ct-point" ct:value="2"></line><line x1="200" y1="367.015" x2="200.01" y2="367.015" class="ct-point" ct:value="1"></line><line x1="350" y1="253.75799999999998" x2="350.01" y2="253.75799999999998" class="ct-point" ct:value="4.7"></line><line x1="500" y1="229.27" x2="500.01" y2="229.27" class="ct-point" ct:value="5.5"></line><line x1="650" y1="152.745" x2="650.01" y2="152.745" class="ct-point" ct:value="8"></line></g></g><g class="ct-labels"><foreignObject style="overflow: visible;" x="50" y="402.625" width="150" height="20"><span class="ct-label ct-horizontal ct-end" xmlns="http://www.w3.org/2000/xmlns/" style="width: 150px; height: 20px;">Понедельник</span></foreignObject><foreignObject style="overflow: visible;" x="200" y="402.625" width="150" height="20"><span class="ct-label ct-horizontal ct-end" xmlns="http://www.w3.org/2000/xmlns/" style="width: 150px; height: 20px;">Вторник</span></foreignObject><foreignObject style="overflow: visible;" x="350" y="402.625" width="150" height="20"><span class="ct-label ct-horizontal ct-end" xmlns="http://www.w3.org/2000/xmlns/" style="width: 150px; height: 20px;">Среда</span></foreignObject><foreignObject style="overflow: visible;" x="500" y="402.625" width="150" height="20"><span class="ct-label ct-horizontal ct-end" xmlns="http://www.w3.org/2000/xmlns/" style="width: 150px; height: 20px;">Четверг</span></foreignObject><foreignObject style="overflow: visible;" x="650" y="402.625" width="30" height="20"><span class="ct-label ct-horizontal ct-end" xmlns="http://www.w3.org/2000/xmlns/" style="width: 30px; height: 20px;">Пятница</span></foreignObject><foreignObject style="overflow: visible;" y="359.3625" x="10" height="38.2625" width="30"><span class="ct-label ct-vertical ct-start" xmlns="http://www.w3.org/2000/xmlns/" style="height: 38px; width: 30px;">0</span></foreignObject><foreignObject style="overflow: visible;" y="321.1" x="10" height="38.2625" width="30"><span class="ct-label ct-vertical ct-start" xmlns="http://www.w3.org/2000/xmlns/" style="height: 38px; width: 30px;">1.25</span></foreignObject><foreignObject style="overflow: visible;" y="282.83750000000003" x="10" height="38.26249999999999" width="30"><span class="ct-label ct-vertical ct-start" xmlns="http://www.w3.org/2000/xmlns/" style="height: 38px; width: 30px;">2.5</span></foreignObject><foreignObject style="overflow: visible;" y="244.57499999999996" x="10" height="38.26250000000002" width="30"><span class="ct-label ct-vertical ct-start" xmlns="http://www.w3.org/2000/xmlns/" style="height: 38px; width: 30px;">3.75</span></foreignObject><foreignObject style="overflow: visible;" y="206.3125" x="10" height="38.26249999999999" width="30"><span class="ct-label ct-vertical ct-start" xmlns="http://www.w3.org/2000/xmlns/" style="height: 38px; width: 30px;">5</span></foreignObject><foreignObject style="overflow: visible;" y="168.05" x="10" height="38.26249999999999" width="30"><span class="ct-label ct-vertical ct-start" xmlns="http://www.w3.org/2000/xmlns/" style="height: 38px; width: 30px;">6.25</span></foreignObject><foreignObject style="overflow: visible;" y="129.78750000000002" x="10" height="38.26249999999999" width="30"><span class="ct-label ct-vertical ct-start" xmlns="http://www.w3.org/2000/xmlns/" style="height: 38px; width: 30px;">7.5</span></foreignObject><foreignObject style="overflow: visible;" y="91.52499999999998" x="10" height="38.262500000000045" width="30"><span class="ct-label ct-vertical ct-start" xmlns="http://www.w3.org/2000/xmlns/" style="height: 38px; width: 30px;">8.75</span></foreignObject><foreignObject style="overflow: visible;" y="53.26249999999999" x="10" height="38.26249999999999" width="30"><span class="ct-label ct-vertical ct-start" xmlns="http://www.w3.org/2000/xmlns/" style="height: 38px; width: 30px;">10</span></foreignObject><foreignObject style="overflow: visible;" y="15" x="10" height="38.26249999999999" width="30"><span class="ct-label ct-vertical ct-start" xmlns="http://www.w3.org/2000/xmlns/" style="height: 38px; width: 30px;">11.25</span></foreignObject><foreignObject style="overflow: visible;" y="-15" x="10" height="30" width="30"><span class="ct-label ct-vertical ct-start" xmlns="http://www.w3.org/2000/xmlns/" style="height: 30px; width: 30px;">12.5</span></foreignObject></g></svg></div>
+</body>
+
 </html>
