cb063ebcdfcf30b9ed57813bcbc113a199686826
DeLi0v <denov78@mail.ru>
2023-05-14 23:18:29 +0300

    modified:   graph.php

--- graph.php
+++ graph.php
@@ -12,7 +12,7 @@
 
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
-          ['Дата', 'Оборудование', 'Количество'],
+          ['Оборудование', 'Количество'],
           <?php require_once("connect.php"); // Подключение файла для связи с БД
             // Подключение к БД
             $db = new DB_Class();
@@ -35,7 +35,7 @@
             $result = mysqli_query($conn, $sql);
             if (mysqli_num_rows($result) > 0) {
                 while ($row = mysqli_fetch_assoc($result)) {
-                echo "['" . $row["data"] . "', '" . $row["name"] . "', ". $row["count"] . "],";
+                echo "['" . $row["name"] . "', ". $row["count"] . "],";
             }
             } ?>
         ]);
