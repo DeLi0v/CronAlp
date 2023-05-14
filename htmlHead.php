5c5109cd4da967e8d0f829ad6aafccfa999b44f3
DeLi0v <denov78@mail.ru>
2023-05-15 02:21:21 +0300

    modified:   htmlHead.php

--- htmlHead.php
+++ htmlHead.php
@@ -3,17 +3,4 @@
     <title>MySiTe</title>
     <link rel="stylesheet" href="/Styles/MainStyles.css">
     <link rel="stylesheet" href="/Styles/AdminPanelStyles.css">
-    
-    <script type="text/javascript" src="jquery.js"></script>
-    <script type="text/javascript">
-        var address = document.location.href;
-
-        if(document.getElementById('a').onclick == true)
-        {
-            alert("The button was pressed");
-            $('a').removeClass('selected');
-            $("a[href='address']").addClass('selected');
-        }
-
-    </script>
 </head>
