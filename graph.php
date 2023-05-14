acd58d1669149980c6b4da80d22717cf8cffc179
DeLi0v <denov78@mail.ru>
2023-05-14 23:09:36 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   graph.php

--- graph.php
+++ graph.php
@@ -1,106 +1,74 @@
 <html>
-  <head>
+
+<head>
     <link rel="stylesheet" href="Styles/MainStyles.css">
     <link rel="stylesheet" href="Styles/AdminPanelStyles.css">
 
-    <!--Load the AJAX API-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-
-      // Load the Visualization API and the controls package.
-      google.charts.load('current', {'packages':['corechart', 'controls']});
-
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawDashboard);
-
-      // Callback that creates and populates a data table,
-      // instantiates a dashboard, a range slider and a pie chart,
-      // passes in the data and draws it.
-      function drawDashboard() {
-
-        // Создание таблицы с данными
-        var data = google.visualization.arrayToDataTable([
-          ['Оборудование', 'Дата', 'Количество'],
-          <?php require_once("connect.php"); // Подключение файла для связи с БД
-            // Подключение к БД
-            $db = new DB_Class();
-            $conn = $db->connect();
-            mysqli_select_db($conn, $db->database);
-            
-            // Запрос
-            $sql = "SELECT 
-                        Equepments.EquepmentName name,
-                        count(Services.idEquepment) count,
-                        DATE_FORMAT(Services.ServiceData, '%d.%m.%Y') data
-                    FROM 
-                        Services
-                        JOIN Equepments on Services.idEquepment = Equepments.idEquepment
-                    WHERE
-                        idOperation = '1'
-                    GROUP BY data, name";
-
-            // Выполняем SQL-запрос
-            $result = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                echo "['" . $row["name"] . "'," . $row["data"] . ", " . $row["count"] . "],";
-            }
-            } ?>
-        ]);
-
-        // Create a dashboard.
-        var dashboard = new google.visualization.Dashboard(
-            document.getElementById('dashboard_div'));
-
-        // Create a range slider, passing some options
-        var Selector = new google.visualization.ControlWrapper({
-          'controlType': 'CategoryFilter',
-          'containerId': 'filter_div',
-          'options': {
-            'filterColumnLabel': 'Оборудование'
-          }
+        google.charts.load('current', {
+            packages: ['bar']
         });
+        var visualization;
 
-        // Create a range slider, passing some options
-        var donutRangeSlider = new google.visualization.ControlWrapper({
-          'controlType': 'NumberRangeFilter',
-          'containerId': 'filter_div',
-          'options': {
-            'filterColumnLabel': 'Дата'
-          }
-        });
+        function draw() {
+            drawVisualization();
+            drawToolbar();
+        }
 
-        // Создание столбчатого графика
-        var Chart = new google.visualization.ChartWrapper({
-          'chartType': 'Bar',
-          'containerId': 'chart_div',
-          'options': {
-            'width': 500,
-            'height': 500,
-            //'pieSliceText': 'value',
-            'legend': 'right'
-          }
-        });
+        function drawVisualization() {
+            var container = document.getElementById('visualization_div');
+            visualization = new google.visualization.Bar(container);
+            new google.visualization.Query('https://spreadsheets.google.com/tq?key=pCQbetd-CptHnwJEfo8tALA').
+            send(queryCallback);
+        }
 
-        // Establish dependencies, declaring that 'filter' drives 'pieChart',
-        // so that the pie chart will only display entries that are let through
-        // given the chosen slider range.
-        dashboard.bind(Selector, Chart);
-        dashboard.bind(donutRangeSlider, Chart);
+        function queryCallback(response) {
+            visualization.draw(response.getDataTable(), {
+                is3D: false
+            });
+        }
 
-        // Draw the dashboard.
-        dashboard.draw(data);
-      }
+        function drawToolbar() {
+            var components = [{
+                    type: 'igoogle',
+                    datasource: 'https://spreadsheets.google.com/tq?key=pCQbetd-CptHnwJEfo8tALA',
+                    gadget: 'https://www.google.com/ig/modules/pie-chart.xml',
+                    userprefs: {
+                        '3d': 1
+                    }
+                },
+                {
+                    type: 'html',
+                    datasource: 'https://spreadsheets.google.com/tq?key=pCQbetd-CptHnwJEfo8tALA'
+                },
+                {
+                    type: 'csv',
+                    datasource: 'https://spreadsheets.google.com/tq?key=pCQbetd-CptHnwJEfo8tALA'
+                },
+                {
+                    type: 'htmlcode',
+                    datasource: 'https://spreadsheets.google.com/tq?key=pCQbetd-CptHnwJEfo8tALA',
+                    gadget: 'https://www.google.com/ig/modules/pie-chart.xml',
+                    userprefs: {
+                        '3d': 1
+                    },
+                    style: 'width: 800px; height: 700px; border: 3px solid purple;'
+                }
+            ];
+
+            var container = document.getElementById('toolbar_div');
+            google.visualization.drawToolbar(container, components);
+        };
+
+        google.charts.setOnLoadCallback(draw);
     </script>
-  </head>
+</head>
 
-  <body>
-  <?php include("head.php"); ?>
-    <!--Div that will hold the dashboard-->
-    <div id="dashboard_div">
-      <!--Divs that will hold each control and chart-->
-      <div id="filter_div"></div>
-      <div id="chart_div"></div>
-    </div>
-  </body>
+<body>
+<?php include("head.php"); ?>
+    <div id="toolbar_div"></div>
+    <div id="visualization_div" style="width: 270px; height: 200px;"></div>
+</body>
+
 </html>
