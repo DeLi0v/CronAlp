67f71006838af6e2e63c9835ec54725d0684350d
DeLi0v <denov78@mail.ru>
2023-05-14 23:40:33 +0300

    Changes to be committed: 	modified:   graph.php

--- graph.php
+++ graph.php
@@ -1,5 +1,6 @@
 <html>
-  <head>
+
+<head>
     <meta charset="utf-8">
     <title>MySiTe</title>
     <link rel="stylesheet" href="Styles/MainStyles.css">
@@ -7,55 +8,63 @@
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-      google.charts.load('current', {'packages':['bar']});
-      google.charts.setOnLoadCallback(drawChart);
+        google.charts.load('current', {
+            'packages': ['bar']
+        });
+        google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Оборудование', 'Количество'],
-          <?php require_once("connect.php"); // Подключение файла для связи с БД
-            // Подключение к БД
-            $db = new DB_Class();
-            $conn = $db->connect();
-            mysqli_select_db($conn, $db->database);
-            
-            // Запрос
-            $sql = "SELECT 
-                        Equepments.EquepmentName name, 
-                        count(Services.idEquepment) count,
-                        DATE_FORMAT(Services.ServiceData, '%d.%m.%Y') data
-                    FROM 
-                        Services
-                        JOIN Equepments on Services.idEquepment = Equepments.idEquepment
-                    WHERE
-                        idOperation = '1'
-                        AND DATE_FORMAT(Services.ServiceData, '%d.%m.%Y') = DATE_FORMAT(NOW(), '%d.%m.%Y') 
-                    GROUP BY data, name";
+        function drawChart() {
+            // Create the data table.
+            var data = new google.visualization.DataTable();
+            data.addColumn('data', 'Дата');
+            data.addColumn('string', 'Оборудование');
+            data.addColumn('number', 'Количество');
+            data.addRows([
+                <?php require_once("connect.php"); // Подключение файла для связи с БД
+                // Подключение к БД
+                $db = new DB_Class();
+                $conn = $db->connect();
+                mysqli_select_db($conn, $db->database);
 
-            // Выполняем SQL-запрос
-            $result = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                echo "['" . $row["name"] . "', ". $row["count"] . "],";
-            }
-            } ?>
-        ]);
+                // Запрос
+                $sql = "SELECT 
+                            Equepments.EquepmentName name, 
+                            count(Services.idEquepment) count,
+                            DATE_FORMAT(Services.ServiceData, '%d.%m.%Y') data
+                        FROM 
+                            Services
+                            JOIN Equepments on Services.idEquepment = Equepments.idEquepment
+                        WHERE
+                            idOperation = '1'
+                            AND DATE_FORMAT(Services.ServiceData, '%d.%m.%Y') = DATE_FORMAT(NOW(), '%d.%m.%Y') 
+                        GROUP BY data, name";
 
-        var options = {
-          chart: {
-            title: 'Количество выданного оборудования',
-            //subtitle: 'Sales, Expenses, and Profit: 2014-2017',
-          }
-        };
+                // Выполняем SQL-запрос
+                $result = mysqli_query($conn, $sql);
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo "['" . $row["data"] . "', '" . $row["name"] . "', ". $row["count"] . "],";
+                    }
+                } ?>
+            ]);
 
-        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+            var options = {
+                chart: {
+                    title: 'Количество выданного оборудования',
+                    //subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                }
+            };
 
-        chart.draw(data, google.charts.Bar.convertOptions(options));
-      }
+            var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+            chart.draw(data, options); //google.charts.Bar.convertOptions(options));
+        }
     </script>
-  </head>
-  <body>
+</head>
+
+<body>
     <?php include("head.php"); ?>
     <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
-  </body>
+</body>
+
 </html>
