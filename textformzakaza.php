327937722c4cfe85b01421970775c3c411c2d9a8
DeLi0v <denov78@mail.ru>
2023-05-16 13:41:32 +0300

    modified:   Styles/MainStyles.css 	modified:   index.php 	new file:   textformzakaza.php

---
+++ textformzakaza.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Заказ подтвержден</title>
+    <link rel="stylesheet" href="Styles/MainStyles.css">
+    <link rel="stylesheet" href="Styles/AdminPanelStyles.css">
+</head>
+
+<body class="services">
+    <?php include("head.php"); ?>
+    <?php 
+    echo "Гриша лох";
+    ?>
+</body>
+
+</html> 
