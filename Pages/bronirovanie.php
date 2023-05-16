e4ad36b462d56c6a892d9db05e0e285ad6706c9e
DeLi0v <denov78@mail.ru>
2023-05-16 21:03:11 +0300

    modified:   Pages/bronirovanie.php

--- Pages/bronirovanie.php
+++ Pages/bronirovanie.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <title>MySiTe</title>
-    <link rel="stylesheet" href="Styles/MainStyles.css">
-    <link rel="stylesheet" href="Styles/AdminPanelStyles.css">
+    <link rel="stylesheet" href="/Styles/MainStyles.css">
+    <link rel="stylesheet" href="/Styles/AdminPanelStyles.css">
 </head>
 
 <body>
