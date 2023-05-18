fd8ff410db50c914430ac5e2a1bd33c6962a1f74
DeLi0v <denov78@mail.ru>
2023-05-19 01:38:03 +0300

    new file:   Styles/slideshow.css 	modified:   htmlHead.php 	modified:   index.php

--- htmlHead.php
+++ htmlHead.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8">
-    <title>MySiTe</title>
+    <title>Альпийская крона</title>
     <link rel="stylesheet" href="/Styles/MainStyles.css">
     <link rel="stylesheet" href="/Styles/AdminPanelStyles.css">
 </head>
