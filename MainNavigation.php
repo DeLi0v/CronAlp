3821d02198f99cf31c6f9d16bf9a47f9ce438483
DeLi0v <denov78@mail.ru>
2023-05-18 18:57:12 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   MainNavigation.php
    	modified:   Styles/MainStyles.css

--- MainNavigation.php
+++ MainNavigation.php
@@ -1,8 +1,4 @@
-<div class="head">
-        <a href="/index.php" style="text-decoration: none;"><h1>Горнолыжный курорт "Альпийская крона"</h1></a>
-</div>
-
-<ul>
+<ul class="mainHead">
   <li><img src="/pictures/Logo_With_Title_Right.png"></li>
   <li><a href="/index.php">Главная</a></li>
   <li><a href="/Pages//Booking/booking.php">Бронирование</a></li>
