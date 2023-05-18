c39609fe4e6770a33b8d7b727eef52e0fb459533
DeLi0v <denov78@mail.ru>
2023-05-18 19:09:52 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   MainHead.php
    	modified:   MainNavigation.php
    	modified:   Styles/MainStyles.css

--- MainNavigation.php
+++ MainNavigation.php
@@ -1,4 +1,4 @@
-<ul class="mainHead">
+<ul class="topnav">
   <li><img src="/pictures/Logo_With_Title_Right.png"></li>
   <li><a href="/index.php">Главная</a></li>
   <li><a href="/Pages//Booking/booking.php">Бронирование</a></li>
