6ca7d1e0c81500aade2881e744a4e01e18f86ee9
DeLi0v <denov78@mail.ru>
2023-05-20 16:28:38 +0300

    modified:   Pages/Account/account.php

--- Pages/Account/account.php
+++ Pages/Account/account.php
@@ -7,26 +7,27 @@
 </head>
 
 <body>
-    <?php 
-    $page = "account"; 
-    if(isset($_GET["no"])){
+    <?php
+    session_name("account");
+    session_start();
+
+    if (isset($_SESSION["LogIn"])) {
+        if ($_SESSION["LogIn"] <> 1) {
+            $_SESSION["LogIn"] = 0;
+        }
+    } else {
+        $_SESSION["LogIn"] = 0;
+    }
+
+    $LogIn = $_SESSION["LogIn"];
+    $page = "account";
+
+    if (isset($_GET["no"])) {
         $err = $_GET["no"];
-        
-        session_name("account");
-        session_start();
-        
-        if(isset($_SESSION["LogIn"])) {
-            if ($_SESSION["LogIn"] <> 1) { 
-                $_SESSION["LogIn"] = 0; 
-            } else {
-                $LogIn = $_SESSION["LogIn"];
-            }
-        } else { $_SESSION["LogIn"] = 0; }
-
-        $log = $_SESSION["LogIn"];
-
-    } else {$err = 0;}?>
-    <?php if($log == 0) { ?>
+    } else {
+        $err = 0;
+    } ?>
+    <?php if ($LogIn == 0) { ?>
         <?php include_once("../../MainNavigation.php") ?>
         <form class="add" action="/Pages/Account/logining.php" method="post" style=" margin:auto; width:500px;">
             <h3 style="text-align:center;">Вход в аккаунт</h3>
@@ -39,17 +40,19 @@
                     <label for="passwd">Пароль:</label>
                     <input type="password" name="passwd" size="20px" />
                 </li>
-                <?php if($err == 1) { ?>
-                <li class="form-row-error">
-                    <label>Неверно введен телефон или пароль</label>
-                </li>
+                <?php if ($err == 1) { ?>
+                    <li class="form-row-error">
+                        <label>Неверно введен телефон или пароль</label>
+                    </li>
                 <?php } ?>
                 <li class="form-row">
                     <button type="submit">Войти</button>
                 </li>
             </ul>
         </form>
-    <?php } else {header("Location: account_page.php");}?>
+    <?php } else {
+        header("Location: account_page.php");
+    } ?>
 </body>
 
-</html>  
+</html>
