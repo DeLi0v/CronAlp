9e599823fed7c026b2eb80ec419e252532291cf9
DeLi0v <denov78@mail.ru>
2023-05-21 02:59:35 +0300

    new file:   Pages/Account/register.php 	new file:   Pages/Account/register_page.php

---
+++ Pages/Account/register.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <?php include_once("../../MainHead.php") ?>
+    <link rel="stylesheet" href="/Styles/AdminPanelStyles.css">
+
+</head>
+
+<body>
+
+    <?php $page = "account"; ?>
+
+    <?php include("../../MainNavigation.php"); ?>
+    <?php
+    if (isset($_POST["Surname"]) && isset($_POST["Name"]) && isset($_POST["Otch"]) && isset($_POST["Phone"]) && isset($_POST["Mail"]) && isset($_POST["passwd"])) {
+        
+        require_once("../../connect.php"); // Подключение файла для связи с БД
+
+        // // Подключение к БД
+        $db = new DB_Class();
+        $conn = $db->connect();
+        mysqli_select_db($conn, $db->database);
+
+        $surname = $conn->real_escape_string($_POST["Surname"]);
+        $name = $conn->real_escape_string($_POST["Name"]);
+        $otch = $conn->real_escape_string($_POST["Otch"]);
+        $phone = $conn->real_escape_string($_POST["Phone"]);
+        $mail = $conn->real_escape_string($_POST["Mail"]);
+        $passwd = $conn->real_escape_string($_POST["passwd"]);
+
+        session_name("register");
+        session_start();
+        $_SESSION["surname"] = $surname;
+        $_SESSION["name"] = $name;
+        $_SESSION["otch"] = $otch;
+        $_SESSION["phone"] = $phone;
+        $_SESSION["mail"] = $mail;
+
+        // Проверка на существование пользователя с таким же телефоном
+        $sql = "SELECT * FROM Clients WHERE Phone = '$phone';";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) { $p=1; } else { $p=0; }
+
+        // Проверка на существование пользователя с такой же почтой
+        $sql = "SELECT * FROM Clients WHERE Phone = '$mail';";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) { $m=1; } else { $m=0; }
+
+        if($p == 1 || $m == 1) { header("Location: register_page.php?p=$p&m=$m"); } // Если есть хотя бы одно совпадение перенаправляем обратно на страницу с регистрацией
+
+        $sql = "INSERT INTO Clients (ClientSurname, ClientName, ClientOtch, Phone, Mail, Passwd) VALUES ('$surname', '$name', '$otch', '$phone', '$mail', '$passwd');";
+        if($conn->query($sql)){
+            echo "<div align=\"center\">
+                <img src=\"/pictures/icons/success.png\" style=\"max-height: 100px;max-width: 100px; padding-top: 15px;\">
+                <div style=\"font-size: 20px;padding-top: 10px;\">Данные успешно добавлены</div>
+            </div>";
+        } else{
+            echo "Ошибка: " . $conn->error;
+        }
+        $conn->close();
+    } else {
+        echo "Что-то не так";
+    }
+    ?>
+
+</body>
+
+</html>
