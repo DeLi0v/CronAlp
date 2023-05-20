81d384ad91771cbb110b1db79b31b1922daaa1c5
DeLi0v <denov78@mail.ru>
2023-05-20 21:18:12 +0300

    modified:   MainNavigation.php

--- MainNavigation.php
+++ MainNavigation.php
@@ -1,12 +1,11 @@
+<ul class="topnav">
+  <li><a href="/index.php" <?php if ($page == "main") { echo "class=\"active\""; } ?>>Главная</a></li>
+  <li><a href="/Pages/Booking/booking_page1.php" <?php if ($page == "booking") { echo "class=\"active\""; } ?>>Бронирование</a></li>
+  <li><a href="#" <?php if ($page == "about") { echo "class=\"active\""; } ?>>О курорте</a></li>
+  <li class="right"><a href="/Pages/Account/account.php" <?php if ($page == "account") { echo "class=\"active\""; } ?>>Личный кабинет</a>
+    <?php if (isset($_SESSION["LogIn"]) && $_SESSION["LogIn"] == 1) { ?>
+      <a href="/Pages/Account/logout.php"><img src="/pictures/icons/logout.png" style="max-width: 25px;border: 0;"></a>
+    <?php } ?>
 
-    <ul class="topnav">
-      <li><a href="/index.php" <?php if($page == "main") {echo "class=\"active\"";}?> >Главная</a></li>
-      <li><a href="/Pages/Booking/booking_page1.php" <?php if($page == "booking") {echo "class=\"active\"";}?> >Бронирование</a></li>
-      <li><a href="#" <?php if($page == "about") {echo "class=\"active\"";}?> >О курорте</a></li>
-      <li class="right"><a href="/Pages/Account/account.php" <?php if($page == "account") {echo "class=\"active\"";}?> >Личный кабинет</a></li>
-      <?php if (isset($_SESSION["LogIn"]) && $_SESSION["LogIn"] == 1) { ?>
-        <li class="right">
-          <a href="/Pages/Account/logout.php"><img src="/pictures/icons/logout.png" style="max-width: 25px;border: 0;"></a>
-        </li>
-      <?php } ?>
-    </ul>
+  </li>
+</ul>
