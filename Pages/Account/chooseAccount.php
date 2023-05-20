6c8f2bcc499598f3d32fcfb35c5ff2974f7b8e05
DeLi0v <denov78@mail.ru>
2023-05-20 18:14:06 +0300

    modified:   Pages/Account/chooseAccount.php

--- Pages/Account/chooseAccount.php
+++ Pages/Account/chooseAccount.php
@@ -14,9 +14,9 @@
 
     <?php include_once("../../MainNavigation.php") ?>
     <h3 style="text-align: center;">Выбери куда зайти!</h3>
-    <div>
-        <a class="choose" href="/Pages/Account/account_page.php">Личный кабинет клиента</a>
-        <a class="choose" href="/Pages/AdminPanel/adminpanel.php">Административная панель</a>
+    <div class="choose">
+        <a href="/Pages/Account/account_page.php">Личный кабинет клиента</a>
+        <a href="/Pages/AdminPanel/adminpanel.php">Административная панель</a>
         <!-- <ul class="choose">
             <li><a href="/Pages/Account/account_page.php">Личный кабинет клиента</a></li>
             <li><a href="/Pages/AdminPanel/adminpanel.php">Административная панель</a></li>
