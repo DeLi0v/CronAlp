ee1ca6e507fbdf27bc60ac96635f377e3d7adc5f
DeLi0v <denov78@mail.ru>
2023-05-20 17:41:21 +0300

    modified:   MainNavigation.php 	modified:   Pages/Account/chooseAccount.php

--- Pages/Account/chooseAccount.php
+++ Pages/Account/chooseAccount.php
@@ -12,13 +12,12 @@
     ?>
 
     <?php include_once("../../MainNavigation.php") ?>
-
     <h3 style="text-align: center;">Выбери куда зайти!</h3>
-    <a herf="/Pages/Account/account_page.php">Личный кабинет клиента</a>
-    <a herf="/Pages/AdminPanel/adminpanel.php">Административная панель</a>
     <div>
-        <a href="/Pages/Account/account_page.php">Личный кабинет клиента</a>
-        <a href="/Pages/AdminPanel/adminpanel.php">Административная панель</a>
+        <ul>
+            <li><a href="/Pages/Account/account_page.php">Личный кабинет клиента</a></li>
+            <li><a href="/Pages/AdminPanel/adminpanel.php">Административная панель</a></li>
+        </ul>
     </div>
 
 </body>
