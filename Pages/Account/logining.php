a8b5753ba06f80343b09117c24966316fd597f0e
DeLi0v <denov78@mail.ru>
2023-05-20 17:06:55 +0300

    modified:   Pages/Account/logining.php

--- Pages/Account/logining.php
+++ Pages/Account/logining.php
@@ -92,7 +92,7 @@
         } elseif ($inClinets == 0 && $inStaff == 1) {
             $_SESSION["LogIn"] = 1;
             header("Location: /Pages/AdminPanel/adminpanel.php");               
-        }
+        } else { header("Location: /Pages/Account/account.php?no=1"); }
     } else { header("Location: /Pages/Account/account.php?no=1"); } ?>
 </body>
 
