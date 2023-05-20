eb21d2ffbc252c2a5794900197e7a05dc56c6b61
DeLi0v <denov78@mail.ru>
2023-05-20 16:13:57 +0300

    modified:   MainNavigation.php

--- MainNavigation.php
+++ MainNavigation.php
@@ -2,6 +2,6 @@
   <li><a href="/index.php" <?php if($page == "main") {echo "class=\"active\"";}?> >Главная</a></li>
   <li><a href="/Pages/Booking/booking_page1.php" <?php if($page == "booking") {echo "class=\"active\"";}?> >Бронирование</a></li>
   <li><a href="#" <?php if($page == "about") {echo "class=\"active\"";}?> >О курорте</a></li>
-  <li><a href="/Pages/Account/account.php" <?php if($page == "account") {echo "class=\"active\"";}?> >Личный кабинет</a></li>
+  <li class="right"><a href="/Pages/Account/account.php" <?php if($page == "account") {echo "class=\"active\"";}?> >Личный кабинет</a></li>
   <!-- <li class="right"><a href="/openAdminPanel.php" <?php if($page == "account") {echo "class=\"active\"";}?> >Админ. панель</a></li> -->
 </ul>
