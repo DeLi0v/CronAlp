906ce6feda841f89c6108ea0d933850779eab82b
DeLi0v <denov78@mail.ru>
2023-05-23 22:16:30 +0300

    modified:   Pages/AdminPanel/Select/EditAndDeleteRows.php

--- Pages/AdminPanel/Select/EditAndDeleteRows.php
+++ Pages/AdminPanel/Select/EditAndDeleteRows.php
@@ -10,8 +10,8 @@ if ($row["id"] == 1 && $page == "Staff") {
     echo "<td class=\"center\">
     <form action='/Pages/AdminPanel/EditPage.php?id=\"".$row["id"]."\"' method=\"post\">
         <input type=\"hidden\" name=\"id\" value=\"".$row["id"]."\">
-        <input type=\"hidden\" name=\"page\" value=\"".$row["operation"]."\">
-        <input type=\"hidden\" name=\"operation\" value=\"$operation\">
+        <input type=\"hidden\" name=\"page\" value=\"$page\">
+        <input type=\"hidden\" name=\"operation\" value=\"".$row["operation"]."\">
         <input type=\"image\" name=\"submit\" value=\"Edit\" src=\"/pictures/icons/edit_orange.png\" style=\"max-width: 30px;border: 0;\">
     </form>
     </td>";
